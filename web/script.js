d67430c62d497b3cf98d592515bc7b0660733034
biancaliebetanz.dev <biancaliebetanz.dev@gmail.com>
2025-08-01 16:39:31 +0000

    minor: adding first tags

---
+++ web/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    function newFacts(){
+        const facts = document.querySelectorAll(".text")
+        console.log(facts)
+    }
+    newFacts();
+})
