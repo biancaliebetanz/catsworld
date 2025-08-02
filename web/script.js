c73c444dac3536d5b5991a163706360327bbf741
biancalbtz <biancalbtz@gmail.com>
2025-08-02 14:04:39 +0000

    fix: importando axios no html e movendo a api

--- web/script.js
+++ web/script.js
@@ -1,8 +1,21 @@
-import { getFact, getFacts } from "../api";
+const http = axios.create({
+    baseURL: "https://catfact.ninja/"
+})
 
-const facts = document.querySelectorAll(".text");
+ async function getFact(){
+    const resp = await http.get("fact");
+    return resp.data.fact;
+}
+
+async function getFacts(){
+    const resp = await http.get("facts");
+    return resp.data.data
+}
+
+const fact = document.querySelector(".text");
 const button = document.querySelector(".btn-new-fact");
 
 button.addEventListener("click", async function() {
-    console.log('oi')
+    newFact = await getFact();
+    fact.textContent = newFact;
     })
