f8c1e944a4e5d1e38e9cd164341ecd2eff1ea17b
biancalbtz <biancalbtz@gmail.com>
2025-08-02 19:12:21 +0000

    minor: adicionando a conexão com api unsplash (incompleto)

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const http = axios.create({
+const catninja = axios.create({
     baseURL: "https://catfact.ninja/"
 })
 
+const unsplash = axios.create({
+    baseURL: "https://api.unsplash.com/",
+    Authorization: "786567 vjj0v8N8bDXq6sDXKLI0Ve0DsUz7HivOCDWKnjq353o"
+})
+
  async function getFact(){
-    const resp = await http.get("fact");
+    const resp = await catninja.get("fact");
     return resp.data.fact;
 }
 
 async function getFacts(){
-    const resp = await http.get("facts");
+    const resp = await catninja.get("facts");
     return resp.data.data
+}
+
+async function getPic(){
+    const pic = await unsplash.get("/photos");
+    console.log(pic)
+    //const pic = await unsplash.get("/photos").url.regular + ("&content-filter=high")
 }
 
 const fact = document.querySelector(".text");
@@ -23,4 +34,5 @@ card.addEventListener("dragstart", async function(){
         fact.textContent = newFact;
         card.classList.remove("fact-box-scroll")
     }, 2000)
+    getPic();
 })
